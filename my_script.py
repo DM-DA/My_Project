7ba25673b875e0e90b819d4a6ac9abc879818b75
DM-DA <pipe1392@hotmail.com>
2024-03-09 10:33:06 -0500

    Prueba push

--- my_script.py
+++ my_script.py
@@ -1,9 +1,9 @@
 import os
 
-print(f'My working directory is {os.getcwd}')
+print(f"My working directory is {os.getcwd}")
 
-x=10
-if x%2==0:
-                print(f"{x} is an even number" )
+x = 10
+if x % 2 == 0:
+    print(f"{x} is an even number")
 else:
-        print(f"{x} is an odd number")
+    print(f"{x} is an odd number")
